9d770385681725a0ed867d7db56a8961ab0614ff
TsToasty <47324110+TsToasty@users.noreply.github.com>
2020-09-15 17:09:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const prefix = '--';
 
 
 
+
 client.once('ready', () =>  {
   console.log('ST2Bot reporting for duty!');
 });
@@ -16,31 +17,33 @@ client.on('message', message => {
   const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
   const user = message.mentions.members.first();
-  const id1 = 188401709721190400;
-  const id2 = 130425663160188928;
+  const id1 = 188401709721190400; //Chris
+  const id2 = 130425663160188928; //Andrew
 
+//Promote Users who type in --agree without the PVT Role
   if(command === 'agree' && !message.member.roles.cache.has('711701131558125660')){
     message.author.send('PVT Role has been assigned!');
     message.member.roles.add('711701131558125660');
   }
-
-  if(command === 'promote' && message.author.id == 'id1' || message.author.id == 'id2'){
-  if(user.roles.cache.some(role => role.name === 'PVT')){
-    user.roles.add('712839907189456986');
-    users.roles.remove('711701131558125660');
-  }
-  if(user.roles.cache.some(role => role.name === 'PFC')){
-    user.roles.add('711701218904506438');
-    users.roles.remove('712839907189456986');
-  }
-  message.channel.send('Command Approved');
-  }
-  if (command === 'promote' && !message.author.id == 'id1' || !message.author.id == 'id2'){
-    message.channel.send('Command Denied');
-  }
-
-
+//Basic Promotion Command executable by Andrew and Chris
+  if(command === 'promote'){
+    if(message.author.id == 'id1' || message.author.id == 'id2'){
+      if(user.roles.cache.some(role => role.name === 'PVT')){
+        user.roles.add('712839907189456986');
+        users.roles.remove('711701131558125660');
+      }
+      if(user.roles.cache.some(role => role.name === 'PFC')){
+        user.roles.add('711701218904506438');
+        users.roles.remove('712839907189456986');
+      }
+      message.channel.send('Command Approved');
+    }
+      else{
+        message.channel.send('Command Denied');
+      }
+    }
 });
+
 
 client.on('guildMemberAdd', newMember => {
   newMember.send('Welcome to the ST2 Server. Please read the message in the Landing-Zone Channel to proceed. Thank you!');

4bf0377c3a407cfd36edad61cc3647ecb024357c
TsToasty <47324110+TsToasty@users.noreply.github.com>
2020-09-17 23:51:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -45,12 +45,12 @@ client.on('message', message => {
     if(command === 'clear'){
       const args = message.content.split(' ').slice(1);
       const amount = args.join(' ');
-      if (!amount) return msg.reply('You haven\'t given an amount of messages which should be deleted!');
-      if (isNaN(amount)) return msg.reply('The amount parameter isn`t a number!');
-      if (amount > 100) return msg.reply('You can`t delete more than 100 messages at once!');
-      if (amount < 1) return msg.reply('You have to delete at least 1 message!');
-      msg.channel.messages.fetch({ limit: amount }).then(messages => {
-        msg.channel.bulkDelete(messages);
+      if (!amount) return message.reply('You haven\'t given an amount of messages which should be deleted!');
+      if (isNaN(amount)) return message.reply('The amount parameter isn`t a number!');
+      if (amount > 100) return message.reply('You can`t delete more than 100 messages at once!');
+      if (amount < 1) return message.reply('You have to delete at least 1 message!');
+      message.channel.messages.fetch({ limit: amount }).then(messages => {
+        message.channel.bulkDelete(messages);
       });
 
     }

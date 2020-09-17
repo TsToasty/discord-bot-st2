041de414a0817fa29cb8e28ec7e788f2fa816d03
TsToasty <47324110+TsToasty@users.noreply.github.com>
2020-09-17 23:44:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -43,9 +43,16 @@ client.on('message', message => {
       }
     }
     if(command === 'clear'){
-      const amount = (message.content - 'clear ');
-      var g = parseInt(amount, 10);
-      message.channel.bulkDelete(g);
+      const args = message.content.split(' ').slice(1);
+      const amount = args.join(' ');
+      if (!amount) return msg.reply('You haven\'t given an amount of messages which should be deleted!');
+      if (isNaN(amount)) return msg.reply('The amount parameter isn`t a number!');
+      if (amount > 100) return msg.reply('You can`t delete more than 100 messages at once!');
+      if (amount < 1) return msg.reply('You have to delete at least 1 message!');
+      await msg.channel.messages.fetch({ limit: amount }).then(messages => {
+        msg.channel.bulkDelete(messages);
+      )});
+
     }
 });
 

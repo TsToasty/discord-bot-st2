2c9d10cae7ebedfc79f97dafc6f08c8e4eaafd0a
TsToasty <47324110+TsToasty@users.noreply.github.com>
2021-03-23 20:52:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -20,30 +20,7 @@ client.on('message', message => {
   const id1 = '188401709721190400'; //Chris
   const id2 = '130425663160188928'; //Andrew
   const id3 = '104757384102068224'; //Toasty
-//Give role when say magic word
-  if(command === 'agree' && !message.member.roles.cache.has('711701131558125660')){
-    message.author.send('PVT Role has been assigned!');
-    message.member.roles.add('711701131558125660');
-    message.channel.bulkDelete(1);
-  }
-//You have rank? You can promot, cool right?
-//This code takes a while to execute, some coffee might help
-  if(command === 'promote'){
-    if(message.author.id === id1 || message.author.id === id2 || message.author.id === id3){
-      if(user.roles.cache.some(role => role.name === 'PVT')){
-        user.roles.add('712839907189456986');
-        user.roles.remove('711701131558125660');
-      }
-      if(user.roles.cache.some(role => role.name === 'PFC')){
-        user.roles.add('711701218904506438');
-        user.roles.remove('712839907189456986');
-      }
-      message.channel.send('<@!' + message.author+ '>' +' just promoted '+ '<@!' + user +'>');
-    }
-      else{
-        message.channel.send('Command Denied');
-      }
-    }
+
 
     if(command === 'bandages'){
       message.channel.send('Bring at least ' + Math.floor(Math.random() * 100000) + '\n You can never have enough');
@@ -66,8 +43,8 @@ client.on('message', message => {
 
 //Does shit when people join server, please no ask, thank!
 client.on('guildMemberAdd', newMember => {
-  newMember.send('Welcome to the ST2 Server. \n Please read the message in the Landing-Zone Channel to proceed. \n Make sure to download the mods, you will find the preset in announcements & server info. \n \n Operations are on Monday, Wednesday, Friday and Sunday at 1600 CET+1 \n Write +-agree in the landing zone channel to be promoted to PVT \n Thank you!');
-
+  newMember.send('Welcome to the ST2 Server. \n Make sure to download the mods, you will find the preset in announcements & server info. \n \n Operations are on Monday, Wednesday, Friday and Sunday at 1900 CET \n I hope you enjoy your stay!');
+  newMember.addRole('711701131558125660'));
 });
 
 //Last Line pls thank

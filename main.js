a57e7f66358b54d0be1627e3b0b46c57efe6e85d
TsToasty <47324110+TsToasty@users.noreply.github.com>
2021-04-13 00:33:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -9,22 +9,13 @@ const prefix = '+-';
 
 
 client.once('ready', () =>  {
-  console.log('ST2Bot reporting for duty!');
+  console.log('Egg');
 });
-client.on('message', message => {
-  if(message.author.id === client.user.id){
-    message.react('🥚');
 
-  }
-  else{
-    return;
-  }
-});
-//Does shit when people join server, please no ask, thank!
-client.on('guildMemberAdd', newMember => {
-  newMember.send('Welcome to the ST2 Server. \n Make sure to download the mods, you will find the preset in announcements & server info. \n \n Operations are on Monday, Wednesday, Friday and Sunday at 1900 CET \n I hope you enjoy your stay');
-  let myRole = client.guild.roles.cache.get('711701131558125660');
-  newMember.roles.add(myRole);
+client.on('message', message => {
+  message.react('🥚');
+
+
 });
 
 //Last Line pls thank
